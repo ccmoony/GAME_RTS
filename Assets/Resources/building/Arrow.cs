437eb6dc631ba776cb1acd341f307706ab8444b7
ccmoony <cyj2003@sjtu.edu.cn>
2024-11-12 14:04:23 +0800

    Add Navigation

--- Assets/Resources/building/Arrow.cs
+++ Assets/Resources/building/Arrow.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
-    public float speed = 10f;        // 箭的飞行速度
-    public float damage = 1f;       // 箭的伤害
-    private Transform target;        // 目标敌人
+    public float speed = 10f;        
+    public float damage = 1f;       
+    private Transform target;        
 
-    // 设置箭的目标
     public void SetTarget(Transform enemy)
     {
         target = enemy;
@@ -18,16 +17,13 @@ public class Arrow : MonoBehaviour
     {
         if (target == null)
         {
-             // 如果目标已经消失，则销毁箭
             Destroy(gameObject);
             return;
         }
 
-        // 让箭朝向目标移动
         Vector3 direction = (target.position - transform.position).normalized;
         float distanceThisFrame = speed * Time.deltaTime;
 
-        // 检查箭是否到达了目标
         if (Vector3.Distance(transform.position, target.position) <= distanceThisFrame)
         {
             HitTarget();
@@ -35,7 +31,6 @@ public class Arrow : MonoBehaviour
         }
         Quaternion lookRotation = Quaternion.LookRotation(direction); 
 
-        // 3. 进行额外的旋转修正，使Y轴指向目标（预制件箭头指向y轴）
         transform.rotation = lookRotation * Quaternion.Euler(90f, 0f, 0f); 
 
         transform.Translate(direction * distanceThisFrame, Space.World);
