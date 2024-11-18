6ca032f7fef2fdd34a6d01848a2c4914a038241b
ccmoony <cyj2003@sjtu.edu.cn>
2024-11-18 18:42:38 +0800

    Add Enemy Attack

--- Assets/Resources/building/resource_building_behavior.cs
+++ Assets/Resources/building/resource_building_behavior.cs
@@ -7,6 +7,7 @@ public class Resource_Building_Behavior : MonoBehaviour
 
     private Currency_Manager currency_Manager;
     public Resource_building_Card card_info;
+    private building_placement manager;
 
     private IEnumerator GenerateCurrency()
     {
@@ -19,10 +20,24 @@ public class Resource_Building_Behavior : MonoBehaviour
             currency_Manager.Change_money(card_info.output_gold);
         }
     }
+
     public void Start()
     {
+        manager = FindObjectOfType<building_placement>();
         currency_Manager=GameObject.Find("Cardbox").GetComponent<Currency_Manager>();
         StartCoroutine(GenerateCurrency());
+    }
+    public void TakeDamage(int attackDamage)
+    {
+        card_info.maximum_HP -= attackDamage;
+    }
+
+
+    void Update(){
+        if (card_info.maximum_HP <= 0){
+            manager.Destroy_Building(gameObject.GetInstanceID());
+            Debug.Log("Building Destroyed");
+        }
     }
 
 }
